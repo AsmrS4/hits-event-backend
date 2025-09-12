f6badebcba7faef3de8d152b1860a796bf769265
Aymir Sunchugashev <as4n.tsu@gmail.com>
2025-09-12 15:41:19 +0700

    modified config file

--- src/main/java/com/backend/event_service/configuration/SecurityConfig.java
+++ src/main/java/com/backend/event_service/configuration/SecurityConfig.java
@@ -36,7 +36,12 @@ public class SecurityConfig {
                 .cors(c -> c.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
                     config.setAllowedOrigins(List.of(
-                            "http://localhost:5500"
+                            "http://localhost:5500",
+                            "http://localhost:5501",
+                            "http://localhost:5502",
+                            "http://localhost:5503",
+                            "http://localhost:5504",
+                            "http://localhost:5505"
                     ));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
